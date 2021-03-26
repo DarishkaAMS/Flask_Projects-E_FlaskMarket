18c70bb180075658b1a1d0f7987b0d0b4808b20b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-26 06:53:50 +0200

    Connect home.html

--- market.py
+++ market.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return '<h2> Hello AMAZING!!! </h2>'
+def home_page_view():
+    return render_template('home.html')
 
