9626269ccfb2ef7bd8bfccf14897639f1c5401a9
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-26 06:47:57 +0200

    Update market.py

--- market.py
+++ market.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h2> Hello AMAZING!!! </h2>'
+
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h2>About something important for {username}</h2>'
