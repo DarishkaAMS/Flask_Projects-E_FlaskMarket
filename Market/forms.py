0cd1fc6e659fa3476f886ecf864ff70cd600de69
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-03-29 17:13:17 +0300

    Update forms.py
    Update labels @ forms.py

--- Market/forms.py
+++ Market/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 
 class RegisterForm(FlaskForm):
-  username = StringField(label='username')
-  email_address = StringField(label='email')
-  password_1 = PasswordField(label='password1')
-  password_2 = PasswordField(label='password2')
+  username = StringField(label='User Name:')
+  email_address = StringField(label='Email Address:')
+  password_1 = PasswordField(label='Password:')
+  password_2 = PasswordField(label='Confirm Password:')
   submit = SubmitField(label='submit')
