56e043a28521d9cfaaa37830be8b7d78dcc4c3ae
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-29 07:13:31 +0300

    Update Project Restructure

---
+++ Market/routes.py
@@ -0,0 +1,16 @@
+from flask import render_template
+
+from Market import app
+from Market.models import Item
+
+
+@app.route('/')
+@app.route('/home')
+def home_page_view():
+    return render_template('home.html')
+
+
+@app.route('/market')
+def market_page_view():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
