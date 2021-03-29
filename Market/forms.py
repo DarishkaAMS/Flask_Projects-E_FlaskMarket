6b4fe8a17ae4123fd5a7f970c10746da62d2cd16
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-03-29 16:23:02 +0300

    Update forms.py
    Add RegisterForm @ forms.py

--- Market/forms.py
+++ Market/forms.py
@@ -1,1 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
 
+class RegisterForm(FlaskForm):
+  username = StringField(label='username')
+  email_address = StringField(label='email')
+  password_1 = PasswordField(label='password1')
+  password_2 = PasswordField(label='password2')
+  submit = SubmitField(label='submit')
