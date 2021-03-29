54975e78f056bb532cb684d2aa13e00d997b3f78
Daryna Nikolaienko <53945387+DarishkaAMS@users.noreply.github.com>
2021-03-29 17:03:42 +0300

    Update __init__.py
    Add SECRET_KEY @ __init__.py

--- Market/__init__.py
+++ Market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'gt563sxkjt66eyhcof22zjl097'
 db = SQLAlchemy(app)
 
 from Market import routes
