512fcfc3c270e43af2d8d0318b4427772ca9b56b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-30 07:36:41 +0300

    Add validators @ forms.py

--- Market/forms.py
+++ Market/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import Length
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
+    username = StringField(label='User Name:', validators=Length(min=2, max=30))
     email_address = StringField(label='Email Address:')
-    password_1 = PasswordField(label='Password:')
+    password_1 = PasswordField(label='Password:', validators=Length(min=8))
     password_2 = PasswordField(label='Confirm Password:')
     submit = SubmitField(label='Create Account')
