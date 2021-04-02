00c0d840c78a612571ee1e1359415d7e014207ee
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-02 18:35:06 +0300

    Add purchase_form @ routes.py

--- Market/routes.py
+++ Market/routes.py
@@ -1,5 +1,5 @@
-from flask import flash, redirect, render_template, url_for
-from flask_login import login_required, login_user, logout_user
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 
 from Market import app, db
 from Market.forms import LoginForm, PurchaseItemForm, RegisterForm, SellItemForm
@@ -12,10 +12,17 @@ def home_page_view():
     return render_template('home.html')
 
 
-@app.route('/market')
+@app.route('/market', methods=['GET', 'POST'])
 @login_required
 def market_page_view():
     purchase_form = PurchaseItemForm()
+    if request.method == "POST":
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            p_item_object.owner = current_user.id
+            current_user.budget -= p_item_object.price
+            db.session.commit()
     items = Item.query.all()
     return render_template('market.html', items=items, purchase_form=purchase_form)
 
